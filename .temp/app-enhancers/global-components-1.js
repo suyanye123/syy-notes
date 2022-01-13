4a2611959648624e27b01ef69c585315db5cbb4e
简素言也 <409914837@qq.com>
2022-01-13 21:52:56 +0800

    /

---
+++ .temp/app-enhancers/global-components-1.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+
+Vue.component("Badge", () => import("E:\\syy-notes\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
+Vue.component("CodeBlock", () => import("E:\\syy-notes\\node_modules\\@vuepress\\theme-default\\global-components\\CodeBlock"))
+Vue.component("CodeGroup", () => import("E:\\syy-notes\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
+
+
+export default {}
